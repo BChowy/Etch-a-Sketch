d43d4ef7e0529bfd4a78cca59b0cb43421c0ca24
Bushra <75831792+BChowy@users.noreply.github.com>
2023-10-29 03:55:05 +0300

    Create divs of the default number
    - Calculate the children dimension before creating them.

--- script.js
+++ script.js
@@ -2,3 +2,13 @@ const container = document.querySelector('#container');
 const resizeBtn = document.querySelector('#resize');
 
 let numOfDiv = 64;
+const dimensions = 960 / numOfDiv;
+    let i = 0;
+    while (i < Math.pow(numOfDiv, 2)) {
+        const div = document.createElement('div');
+        div.classList.toggle('divStyle');
+        div.style.height = dimensions + 'px';
+        div.style.width = dimensions + 'px';
+        container.appendChild(div);
+        i++;
+    }
