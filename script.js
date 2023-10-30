64f504783340983047a49850bd359d52c3df44a7
Bushra <75831792+BChowy@users.noreply.github.com>
2023-10-30 04:04:43 +0300

    Wrap creating grid of div into a function
    - Called the function createGrid.
    - Calculated the number of pixels outside the loop.

--- script.js
+++ script.js
@@ -5,14 +5,21 @@ const resizeBtn = document.querySelector('#resize');
 //Calculate dimension
 let calculateDims = (divPerRow) => 960 / divPerRow;
 
+//Create a grid of div
+function createGrid(divPerRow = 32) {
+    const dimensions = calculateDims(divPerRow);
+    let pixels = Math.pow(divPerRow, 2);
     let i = 0;
-    while (i < Math.pow(numOfDiv, 2)) {
+
+    //create div, add class to it, determine it dimensions, then append it to the container
+    while (i < pixels) {
         const div = document.createElement('div');
         div.classList.toggle('divStyle');
         div.style.height = dimensions + 'px';
         div.style.width = dimensions + 'px';
         container.appendChild(div);
         i++;
+    }
     }
 
 const allDiv = container.querySelectorAll('div');
